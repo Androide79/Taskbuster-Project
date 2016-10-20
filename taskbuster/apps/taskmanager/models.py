db6fc000de7ff2202ee7b43b220394b84bd66bad
Pierluigi D'Antona <pierluigi@crazygeek.it>
2016-10-21 01:11:43 +0200

    Taskmanager App creation

---
+++ taskbuster/apps/taskmanager/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class MyModel(models.Model):
+    # Relations
+    # Attributes - Mandatory
+    # Attributes - Optional
+    # Object Manager
+    # Custom Properties
+    # Methods
+    # Meta and String
--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taskbuster',
+    'taskbuster.apps.taskmanager',
 ]
 
 INSTALLED_APPS += [
