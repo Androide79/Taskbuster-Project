317cf4aa547e983c9b3836f6aa5b31d0917be73f
Pierluigi D'Antona <pierluigi@crazygeek.it>
2016-10-23 14:55:39 +0200

    Profile Model and Profile Istance creation

--- taskbuster/apps/taskmanager/admin.py
+++ taskbuster/apps/taskmanager/admin.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-
-# Register your models here.
+from . import models
+ 
+ 
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+ 
+    list_display = ("username", "interaction")
+ 
+    search_fields = ["user__username"]
--- taskbuster/apps/taskmanager/models.py
+++ taskbuster/apps/taskmanager/models.py
@@ -1,11 +1,49 @@
+# -*- coding: utf-8 -*-
 from django.db import models
-
-# Create your models here.
-class MyModel(models.Model):
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+ 
+from . import managers
+ 
+ 
+class Profile(models.Model):
     # Relations
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name="profile",
+        verbose_name=_("user")
+        )
     # Attributes - Mandatory
+    interaction = models.PositiveIntegerField(
+        default=0,
+        verbose_name=_("interaction")
+        )
     # Attributes - Optional
     # Object Manager
+    objects = managers.ProfileManager()
+ 
     # Custom Properties
+    @property
+    def username(self):
+        return self.user.username
+ 
     # Methods
+ 
     # Meta and String
+    class Meta:
+        verbose_name = _("Profile")
+        verbose_name_plural = _("Profiles")
+        ordering = ("user",)
+ 
+    def __str__(self):
+        return self.user.username
+
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+ 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile_for_new_user(sender, created, instance, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
